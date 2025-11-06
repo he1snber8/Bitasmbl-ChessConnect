437217552db76e18371fca7f236171c68750273e
he1snber8 <116442582+he1snber8@users.noreply.github.com>
2025-11-06 14:38:32 +0400

    Implement Socket.IO connection in ChessBoard
    Connect to Socket.IO and handle board updates.

--- src/components/ChessBoard.jsx
+++ src/components/ChessBoard.jsx
@@ -1,1 +1,34 @@
-// src/components/ChessBoard.jsx\nimport React, { useEffect } from 'react';\nimport io from 'socket.io-client';\n// TODO: connect to Socket.IO and render chessboard UI\nexport default function ChessBoard() { return <div id=\"board\" />; }
+import { useEffect } from "react";
+import { io } from "socket.io-client";
+
+
+export default function ChessBoard() {
+useEffect(() => {
+const socket = io("http://localhost:4000");
+socket.emit("join_game");
+
+
+socket.on("board_update", (state) => {
+console.log("Board updated:", state);
+});
+
+
+return () => socket.disconnect();
+}, []);
+
+
+return (
+<div className="grid grid-cols-8 w-80 h-80">
+{Array.from({ length: 64 }).map((_, idx) => (
+<div
+key={idx}
+className={`border border-gray-700 ${
+Math.floor(idx / 8) % 2 === idx % 2
+? "bg-gray-300"
+: "bg-gray-600"
+}`}
+/>
+))}
+</div>
+);
+}
